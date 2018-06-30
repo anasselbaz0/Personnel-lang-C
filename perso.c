205491535ef821d33df3ad7541b3014e6738ce95
anasselbaz0 <anasselbaz0@gmail.com>
2018-06-30 13:37:17 +0100

    AjoutPersonne modifiée
    La fonction ajoute les variable au structure, or il ne place pas le maillon au queue du liste.

--- perso.c
+++ perso.c
@@ -1,53 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define TAILLE_ID 4
 #include "perso.h"
 
 int main(){
-	printf("   *** Gestion de ETUDIANTS ***");
 	short rep=1;
 	int choix;
 	do{
+		system("clear");
 		afficherMenu();
 		printf("Votre choix est : ");
 		scanf("%d",&choix);
 		switch (choix){
 			case 0:
-				return 1;
+				rep=0;
 				break;
 			case 1: //Ajouter une personne
-				system("clear");printf("   *** Gestion de personnel ***\n\n");
-				printf("  ==> Ajout d'un personne:");
-				if(ajouterPersonne()){
-					printf("Personne ajoute avec sucsses!");
-				} //avec une structure Personne
+				system("clear");
+				int m = ajouterPersonne();
+				if(m == 1){
+					printf("Personne ajoute!");
+				}
 				else{
-					printf("Personne non ajoute :(");
+					printf("Ajout non accorde!");
 				}
 				rep=1;
 				break;
 			case 2: //Supprimer une personne
-				system("cls");printf("   *** Gestion de personnel ***\n\n");
-				printf("  ==> Suppression d'un personne:");
-				printf("Nom du personne a supprimer : ");
+				system("clear");
+				printf("   *** Gestion de personnel ***\n\n");
+				printf("==> Suppression d'une personne:\n");
+				printf("  Nom du personne a supprimer : ");
 				char* p_nom;
 				scanf("%s",p_nom);
-				if(supprimerPersonne(p_nom)){
-					printf("Personne supprime avec sucsses!");
-				} //avec une structure Personne
-				else{
-					printf("Personne non supprime :(");
-				}
+				supprimerPersonne(p_nom);
 				free(p_nom);
 				rep=1;
 				break;
-			case 3://Mise a jour
-				system("cls");printf("   *** Gestion de personnel ***\n\nHELLO HHHHHHHH");
+			case 3: //Mise a jour
+				system("clear");
+				printf("   *** Gestion de personnel ***\n\n");
 				rep=1;
 				break;
 			case 4://Impression des donnees
+				system("clear");
+				printf("   *** Gestion de personnel ***\n\n");
 				rep=1;
 				break;
 		}
 	}while(rep==1);
+	printf("Vous allez quitter le systeme ...");
 	return 0;
 }
