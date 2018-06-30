524208b6ef8e063c2acb4436b9031891261f610a
anasselbaz0 <anasselbaz0@gmail.com>
2018-06-30 14:56:10 +0100

    ajout a la liste modifié

--- perso.h
+++ perso.h
@@ -8,7 +8,7 @@ typedef struct Personne{
 	struct Personne* suivant;
 }Personne;
 
-Personne A; //liste chaine ou on va stocker les donnees
+
 
 void afficherMenu(){
 	printf("		*** Gestion de personnel ***");
@@ -19,19 +19,22 @@ void afficherMenu(){
 	printf("\n      0- Quitter\n\n");
 }
 
-int ajouterPersonne(){  // Ajout au queue du liste chainee
+
+
+int ajouterPersonne(Personne *A){  // Ajout au queue du liste chainee A
 	int r=0;
 	printf("   *** Gestion de personnel ***\n\n");
 	printf("==> Ajout d'une personne:");
-	Personne * p;
+	Personne* p = NULL;
+	p = (Personne*)malloc(sizeof(Personne));
 	//Ajout des valeurs
 	printf("\n");
 	p->nom    = (char*)malloc(20 * sizeof(char));
 	p->prenom = (char*)malloc(20 * sizeof(char));
 	p->sexe   = (char*)malloc(10 * sizeof(char));
-	printf("  Nom = ");                 scanf("%s",p->nom);	
-	printf("  Prenom = ");              scanf("%s",p->prenom);
-	printf("  Sexe = ");                scanf("%s",p->sexe);
+	printf("  Nom              = ");    scanf("%s",p->nom);	
+	printf("  Prenom           = ");    scanf("%s",p->prenom);
+	printf("  Sexe             = ");    scanf("%s",p->sexe);
 	printf("  Nombre d enfants = ");    scanf("%d",&p->nbr_enfant);
 	int n = p->nbr_enfant;
 	if(n < 0){ //si c'est negatif, on le considere comme nulle
@@ -53,19 +56,25 @@ int ajouterPersonne(){  // Ajout au queue du liste chainee
 	p->id = (char*)malloc(TAILLE_ID * sizeof(char));
 	printf("  Identifient personnel (%d caracteres) = ",TAILLE_ID);   scanf("%s",p->id);
 	//Ajout a la liste
-	printf("dddd");
-	Personne * tmp = &A;
-	while(tmp->suivant != NULL){
-		tmp = tmp->suivant;
-	}
-	tmp->suivant = p;
 	p->suivant = NULL;
+	if(A == NULL){
+		A = p;
+	}
+	else{
+		Personne *tmp = A;
+		while(tmp->suivant != NULL){
+			tmp = tmp->suivant;
+		}
+		tmp->suivant = p;
+	}
 	r=1;
 	return r;
 }
 
-void supprimerPersonne(char* p_nom){
-	Personne * tmp = &A;
+
+
+void supprimerPersonne(Personne* A, char* p_nom){
+	Personne * tmp = A;
 	printf("Hi");
 	while((tmp->suivant->nom != p_nom) && (tmp->suivant != NULL)){
 		tmp = tmp->suivant;
@@ -74,3 +83,4 @@ void supprimerPersonne(char* p_nom){
 	tmp->suivant = tmp->suivant->suivant;
 	free(a_supp);
 }
+
