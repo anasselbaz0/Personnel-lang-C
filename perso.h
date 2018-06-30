b12df5f809e3e714f8ee243049835f58fdc595ac
anasselbaz0 <anasselbaz0@gmail.com>
2018-06-30 13:38:19 +0100

    AjoutePersonne modifié.

--- perso.h
+++ perso.h
@@ -1,57 +1,76 @@
-struct Personne{
-	char*  nom;
-	char*  prenom;
-	char*  sexe;
-	int    nbr_enfant;
-	char** enfants;
-	char*  id;
+typedef struct Personne{
+	char*            nom;
+	char*            prenom;
+	char*            sexe;
+	int              nbr_enfant;
+	char**           enfants;
+	char*            id;
 	struct Personne* suivant;
-};
+}Personne;
 
-struct Personne A;
+Personne A; //liste chaine ou on va stocker les donnees
 
 void afficherMenu(){
-	printf("\n\n    1- Ajouter   une personne");
-	printf("\n    2- Supprimer une personne");
-	printf("\n    3- Mise a jour d'une personne");
-	printf("\n    4- Imprimer les donnees");
-	printf("\n    0- Quitter\n\n");
+	printf("		*** Gestion de personnel ***");
+	printf("\n\n      1- Ajouter       une personne");
+	printf("\n      2- Supprimer     une personne");
+	printf("\n      3- Mise a jour d'une personne");
+	printf("\n      4- Imprimer les donnees");
+	printf("\n      0- Quitter\n\n");
 }
 
-int ajouterPersonne(){
-	struct Personne p;
-	printf("\n  Nom = ");            scanf("%s",&p.nom[0]);
-	printf("  Prenom = ");           scanf("%s",&p.prenom[0]);
-	printf("  Sexe = ");             scanf("%s",p.sexe); //scanf("%s",&p.sexe[0]); 
-	printf("  Nombre d enfants = "); scanf("%d",&p.nbr_enfant);
-	int n = p.nbr_enfant;
-	if(n > 0){
-		int i = 0;
-		p.enfants = (char**)malloc(n * sizeof(char*));
-		while(i < n){
-			printf("Entrer l'id du %deme enfant : ",i);
-			scanf("%s",p.enfants[i]);
-			i++;
+int ajouterPersonne(){  // Ajout au queue du liste chainee
+	int r=0;
+	printf("   *** Gestion de personnel ***\n\n");
+	printf("==> Ajout d'une personne:");
+	Personne * p;
+	//Ajout des valeurs
+	printf("\n");
+	p->nom    = (char*)malloc(20 * sizeof(char));
+	p->prenom = (char*)malloc(20 * sizeof(char));
+	p->sexe   = (char*)malloc(10 * sizeof(char));
+	printf("  Nom = ");                 scanf("%s",p->nom);	
+	printf("  Prenom = ");              scanf("%s",p->prenom);
+	printf("  Sexe = ");                scanf("%s",p->sexe);
+	printf("  Nombre d enfants = ");    scanf("%d",&p->nbr_enfant);
+	int n = p->nbr_enfant;
+	if(n < 0){ //si c'est negatif, on le considere comme nulle
+		p->nbr_enfant = 0;
+		printf("  Nombre d enfants = 0");
+	}
+	else{ // n >= 0
+		if(n > 0){  // n != 0
+			int i = 0;
+			p->enfants = (char**)malloc(n * sizeof(char*));
+			while(i < n){
+				p->enfants[i] = (char*)malloc(TAILLE_ID * sizeof(char));
+				printf("   -Entrer l'id du **%d** enfant : ",i+1);
+				scanf("%s",p->enfants[i]);
+				i++;
+			}
 		}
 	}
-	printf("  ID = "); scanf("%s",p.id);
-	struct Personne* tmp = &A;
+	p->id = (char*)malloc(TAILLE_ID * sizeof(char));
+	printf("  Identifient personnel (%d caracteres) = ",TAILLE_ID);   scanf("%s",p->id);
+	//Ajout a la liste
+	printf("dddd");
+	Personne * tmp = &A;
 	while(tmp->suivant != NULL){
 		tmp = tmp->suivant;
 	}
-	tmp->suivant = &p;
-	p.suivant = NULL;
-	return 1;
+	tmp->suivant = p;
+	p->suivant = NULL;
+	r=1;
+	return r;
 }
 
-int supprimerPersonne(char* p_nom){
-	struct Personne* tmp = &A;
+void supprimerPersonne(char* p_nom){
+	Personne * tmp = &A;
+	printf("Hi");
 	while((tmp->suivant->nom != p_nom) && (tmp->suivant != NULL)){
 		tmp = tmp->suivant;
 	}
-	struct Personne* supp = tmp;
+	Personne *a_supp = tmp;
 	tmp->suivant = tmp->suivant->suivant;
-	free(tmp);
-	return 1;
+	free(a_supp);
 }
-
